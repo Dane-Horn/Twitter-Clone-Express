c815cb92ff5588159562b5425cf11b5dabf59ced
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-23 21:35:54 +0200

    create user-tweet relation

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,15 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-    id: DataTypes.UUID,
-    user_id: DataTypes.UUID,
-    text: DataTypes.TEXT,
-    likes: DataTypes.INTEGER,
-    references: DataTypes.UUID,
-    created_at: DataTypes.DATE
-  }, {});
-  Tweet.associate = function(models) {
-    // associations can be defined here
-  };
-  return Tweet;
+    const Tweet = sequelize.define('Tweet', {
+        id: DataTypes.UUID,
+        user_id: DataTypes.UUID,
+        text: DataTypes.TEXT,
+        likes: DataTypes.INTEGER,
+        references: DataTypes.UUID,
+        created_at: DataTypes.DATE
+    }, {});
+    Tweet.associate = function (models) {
+        // associations can be defined here
+        Tweet.belongsTo(models.User)
+    };
+    return Tweet;
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: DataTypes.UUID,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    salt: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
+    const User = sequelize.define('User', {
+        id: DataTypes.UUID,
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        salt: DataTypes.STRING
+    }, {});
+    User.associate = function (models) {
+        // associations can be defined here
+        User.hasMany(models.Tweet)
+    };
+    return User;
 };
