004b372ac482c7facc55593d346a5774b2aec0e5
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-23 22:10:15 +0200

    fix migrations

--- src/migrations/20190322142324-create-user.js
+++ src/migrations/20190322142324-create-user.js
@@ -4,20 +4,25 @@ module.exports = {
         return queryInterface.createTable('Users', {
             id: {
                 allowNull: false,
-                autoIncrement: true,
                 primaryKey: true,
                 type: Sequelize.UUID
             },
             username: {
+                allowNull: false,
+                unique: true,
                 type: Sequelize.STRING
             },
             email: {
+                allowNull: false,
+                unique: true,
                 type: Sequelize.STRING
             },
             password: {
+                allowNull: false,
                 type: Sequelize.STRING
             },
             salt: {
+                allowNull: false,
                 type: Sequelize.STRING
             },
             createdAt: {
