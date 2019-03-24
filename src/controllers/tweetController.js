726c581af5578b87433550c9c9443e79e95dd563
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-24 15:58:59 +0200

    Add tweet creation

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -3,5 +3,16 @@ const User = require('../models').User;
 const uuid = require('uuid/v1');
 
 module.exports = {
-
+    async create(req, res) {
+        try {
+            req.body.user_id = req.userID;
+            req.body.id = uuid();
+            let fields = Object.keys(req.body);
+            let tweet = await Tweet.create(req.body, { fields });
+            return res.status(201).send();
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ message: 'Internal server error' });
+        }
+    }
 };
--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -2,5 +2,5 @@ const tweetController = require('../controllers/tweetController');
 const checkAuth = require('../auth/check-auth');
 
 module.exports = (app) => {
-
+    app.post('/tweet', checkAuth, tweetController.create);
 };
