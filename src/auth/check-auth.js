686b0759b76b4fbe1be2e9c78245550cab4151f6
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-24 13:43:26 +0200

    Adjust auth to require bearer format

--- src/auth/check-auth.js
+++ src/auth/check-auth.js
@@ -2,8 +2,10 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const decoded = jwt.verify(req.headers.authorization, 'secret');
-        req.userData = decoded;
+        authHeader = req.headers.authorization.slice(7);
+        console.log(authHeader)
+        const decoded = jwt.verify(authHeader, 'secret');
+        req.userID = decoded.id;
         next();
     } catch (error) {
         return res.status(401).send({ message: 'Auth failed' });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,4 @@ const checkAuth = require('../auth/check-auth');
 module.exports = (app) => {
     app.get('/user/register', userController.create);
     app.get('/user/login', userController.login);
-    app.get('/user', checkAuth, (req, res) => {
-        res.status(200).send("Authorized");
-    });
 }
