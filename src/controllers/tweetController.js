2d3c2d1d60a36ad86ec9ecdc13ad6b9297396813
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-24 21:58:38 +0200

    route to create reply to a tweet

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -11,7 +11,21 @@ module.exports = {
             let tweet = await Tweet.create(req.body, { fields });
             return res.status(201).send();
         } catch (error) {
-            console.log(error);
+            return res.status(500).send({ message: 'Internal server error' });
+        }
+    },
+    async createReply(req, res) {
+        try {
+            let replyTweet = await Tweet.findOne({ where: { id: req.params.id } });
+            if (!replyTweet)
+                res.status(400).send({ message: 'Invalid tweet' });
+            req.body.references = req.params.id;
+            req.body.user_id = req.userID;
+            req.body.id = uuid();
+            let fields = Object.keys(req.body);
+            let reply = await Tweet.create(req.body, { fields });
+            return res.status(201).send();
+        } catch (error) {
             return res.status(500).send({ message: 'Internal server error' });
         }
     }
--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -3,4 +3,5 @@ const checkAuth = require('../auth/check-auth');
 
 module.exports = (app) => {
     app.post('/tweet', checkAuth, tweetController.create);
+    app.post('/tweet/:id/reply', checkAuth, tweetController.createReply);
 };
