c3bbb08123d963c82d28a142c6e07ab4a864a693
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-27 21:12:46 +0200

    reorder routes

--- src/routes/tweetRoutes.js
+++ src/routes/tweetRoutes.js
@@ -2,7 +2,7 @@ const tweetController = require('../controllers/tweetController');
 const checkAuth = require('../auth/check-auth');
 
 module.exports = (app) => {
-    app.post('/tweet', checkAuth, tweetController.create);
     app.post('/tweet/:id/reply', checkAuth, tweetController.createReply);
     app.delete('/tweet/:id', checkAuth, tweetController.delete);
+    app.post('/tweet', checkAuth, tweetController.create);
 };
