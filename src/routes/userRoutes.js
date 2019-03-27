3576162ed0e11941fdc615c083974a64ec556a2f
Dane Horn <g16H0473@ict.ru.ac.za>
2019-03-27 21:13:26 +0200

    reorder routes and add route for others' posts

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,10 +4,11 @@ const checkAuth = require('../auth/check-auth');
 module.exports = (app) => {
     app.delete('/user/deregister', checkAuth, userController.delete);
     app.get('/user/following', checkAuth, userController.following);
-    app.post('/user/register', userController.create);
-    app.post('/user/login', userController.login);
     app.post('/follow/:id', checkAuth, userController.follow);
     app.delete('/unfollow/:id', checkAuth, userController.unfollow);
-    app.get('/posts/own', checkAuth, userController.ownPosts);
     app.get('/feed', checkAuth, userController.feed);
+    app.get('/posts/own', checkAuth, userController.ownPosts);
+    app.post('/user/register', userController.create);
+    app.post('/user/login', userController.login);
+    app.post('/posts/:id');
 }
