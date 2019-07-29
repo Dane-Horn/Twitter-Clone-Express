5eec6f956e233019bfbd1925620eda5fac989738
Dane Horn <g16H0473@campus.ru.ac.za>
2019-07-29 13:22:25 +0200

    fix modified routes

--- src/routes/retweetRoutes.js
+++ src/routes/retweetRoutes.js
@@ -2,6 +2,6 @@ const retweetController = require('../controllers/retweetController');
 const checkAuth = require('../auth/check-auth');
 
 module.exports = (app) => {
-    app.post('api/retweet/:id', checkAuth, retweetController.create);
-    app.delete('api/retweet/:id', checkAuth, retweetController.delete);
+    app.post('/api/retweet/:id', checkAuth, retweetController.create);
+    app.delete('/api/retweet/:id', checkAuth, retweetController.delete);
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,13 +2,13 @@ const userController = require('../controllers/userController');
 const checkAuth = require('../auth/check-auth');
 
 module.exports = (app) => {
-    app.delete('api/user/deregister', checkAuth, userController.delete);
-    app.get('api/user/following', checkAuth, userController.following);
-    app.post('api/follow/:id', checkAuth, userController.follow);
-    app.delete('api/unfollow/:id', checkAuth, userController.unfollow);
-    app.get('api/feed', checkAuth, userController.feed);
-    app.get('api/posts/own', checkAuth, userController.ownPosts);
-    app.post('api/user/register', userController.create);
-    app.post('api/user/login', userController.login);
-    app.post('api/posts/:id');
+    app.delete('/api/user/deregister', checkAuth, userController.delete);
+    app.get('/api/user/following', checkAuth, userController.following);
+    app.post('/api/follow/:id', checkAuth, userController.follow);
+    app.delete('/api/unfollow/:id', checkAuth, userController.unfollow);
+    app.get('/api/feed', checkAuth, userController.feed);
+    app.get('/api/posts/own', checkAuth, userController.ownPosts);
+    app.post('/api/user/register', userController.create);
+    app.post('/api/user/login', userController.login);
+    app.post('/api/posts/:id');
 }
