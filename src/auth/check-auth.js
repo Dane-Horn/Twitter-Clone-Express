22bfca61802dd88199b8f9e286f6c08119c71cbd
Dane Horn <g16H0473@campus.ru.ac.za>
2019-07-29 13:18:04 +0200

    change to bcryptjs - exactly the same

--- src/auth/check-auth.js
+++ src/auth/check-auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         authHeader = req.headers.authorization.slice(7);
-        console.log(authHeader)
         const decoded = jwt.verify(authHeader, 'secret');
         req.userID = decoded.id;
         next();
